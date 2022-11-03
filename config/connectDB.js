ea3fb61e86c1e677cfa52cc90e4befc7f5630dde
tinku gupta <tinku.gupta4@gmail.com>
2022-11-04 01:51:46 +0530

    first commit

---
+++ config/connectDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const colors = require('color')
+
+// craete a function to connect with database 
+
+const connectDb = async () => {
+    try {
+
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Server Running on ${mongoose.connection.host}`.bgCyan.white)
+        
+    } catch (error) {
+        console.log(`tttttt ${error}`.bgRed);
+    }
+}
+
+
+module.exports = connectDb
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('color')
+const connectDB = require("./config/connectDB")
+
+//CONFIG DOT ENV FILE
+
+dotenv.config();
+
+// database call
+connectDB()
+
+// rest object
+const app = express();
+
+//middlewares
+
+app.use(morgan("dev"));
+app.use(express.json())
+app.use(cors());
+
+//routes
+
+app.get("/", (req,res) => {
+    res.send("<h1>Hellow form server</h1>")
+})
+
+//port 
+
+const PORT = 8080 || process.env.PORT;
+
+// LISTEN SERVER
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+})
+
