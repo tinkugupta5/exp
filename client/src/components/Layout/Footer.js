59c68159180228a8acd69072f427f5af36e19309
tinku gupta <tinku.gupta4@gmail.com>
2022-12-01 00:34:43 +0530

    change the data

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-4'>
+    <div className='bg-light text-light p-4'>
       <h6 className='text-center'>All right reserved ziontutorial &copy; ziontutorial</h6>
     </div>
   )
