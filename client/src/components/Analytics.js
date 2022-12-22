1054b3f2c2603b0c279fc9acb14b80256c2af0f7
tinku gupta <tinku.gupta4@gmail.com>
2022-12-23 01:38:04 +0530

    syntax

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Analytics = ({allTransection}) => {
 
   const totalTransaction = allTransection.length
+  console.log("this is analytics value",totalTransaction);
   const totalIncomeTransactions = allTransection.filters(transaction => transaction ==='income')
   const totalExpenseTransactions = allTransection.filters(transaction => transaction ==='expense')
+  const totalIncomePercent = (totalIncomeTransactions/totalTransaction)*100
+  const totalExpensePercent = (totalIncomePercent/totalTransaction)*100
 
 
 
@@ -12,6 +15,19 @@ const Analytics = ({allTransection}) => {
   return (
     <div>
       <h1>This is new</h1>
+
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='card'>
+            <div className='card-header'>
+              Total Transactions : {totalTransaction}
+            </div>
+            <div className='card-body'>
+              <h5>Income : {totalIncomeTransactions}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* 10:56 */}
     </div>
