a3a6816a9d401058fd212ade24482bf1f49a99fd
tinku gupta <tinku.gupta4@gmail.com>
2022-12-24 20:05:33 +0530

    adding progress bar in analytical view

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Progress, Space } from 'antd';
 
 const Analytics = ({allTransection}) => {
 
   const totalTransaction = allTransection.length
   console.log("this is analytics value",totalTransaction);
-  const totalIncomeTransactions = allTransection.filters(transaction => transaction ==='income')
-  const totalExpenseTransactions = allTransection.filters(transaction => transaction ==='expense')
-  const totalIncomePercent = (totalIncomeTransactions/totalTransaction)*100
-  const totalExpensePercent = (totalIncomePercent/totalTransaction)*100
+  const totalIncomeTransactions = allTransection.filter(transaction => transaction.type ==='income')
+  const totalExpenseTransactions = allTransection.filter(transaction => transaction.type ==='expense')
+  const totalIncomePercent = (totalIncomeTransactions.length/totalTransaction)*100
+  const totalExpensePercent =(totalExpenseTransactions.length / totalTransaction) * 100;
 
 
 
@@ -23,13 +24,18 @@ const Analytics = ({allTransection}) => {
               Total Transactions : {totalTransaction}
             </div>
             <div className='card-body'>
-              <h5>Income : {totalIncomeTransactions}</h5>
+              <h5 className='text-success'>Income : {totalIncomeTransactions.length}</h5>
+              <h5 className='text-danger'>Expense : {totalExpenseTransactions.length}</h5>
+              <div>
+              <Progress type="circle" strokeColor={'green'} percent={totalIncomePercent.toFixed(0)} className="mx-2" />
+              <Progress type="circle" strokeColor={'red'} percent={totalExpensePercent.toFixed(0)} className="mx-2 " />
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      {/* 10:56 */}
+      {/* 14:32 */}
     </div>
   )
 }
